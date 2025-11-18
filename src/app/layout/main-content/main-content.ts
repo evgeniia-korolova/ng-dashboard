823b00c3635c5e138ef124d0c2b75a2c3b2e9a68
evgeniia-korolova <jkod@ukr.net>
2025-11-18 02:37:47 +0200

    feat: add transition

--- src/app/layout/main-content/main-content.ts
+++ src/app/layout/main-content/main-content.ts
@@ -1,13 +1,12 @@
-import { BreakpointObserver } from '@angular/cdk/layout';
-import { Component, computed, effect, inject, signal, viewChild } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { MatIconButton, MatAnchor } from '@angular/material/button';
+import { Component, computed, signal } from '@angular/core';
+
+import { MatIconButton } from '@angular/material/button';
 import { MatDivider } from '@angular/material/divider';
 import { MatIcon } from '@angular/material/icon';
 import { MatDrawerContainer, MatDrawer, MatDrawerContent } from '@angular/material/sidenav';
 import { MatToolbar } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
-import { map } from 'rxjs';
+
 import { CustomSidenav } from '../../features/custom-sidenav/custom-sidenav';
 
 @Component({
@@ -29,4 +28,5 @@ export class MainContent {
   collapsed = signal(false);
 
   sidenavWidth = computed(() => (this.collapsed() ? '72px' : '250px'));
+  contentWidth = computed(() => (this.collapsed() ? '82px' : '260px'));
 }
