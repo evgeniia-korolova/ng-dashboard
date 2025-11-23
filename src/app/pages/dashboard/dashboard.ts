7265143abb7f971bea6ed3941d52950fdf4b0b42
evgeniia-korolova <jkod@ukr.net>
2025-11-23 23:52:05 +0200

    feat: add widgets animation with animate-css-grid packege

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,9 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, viewChild } from '@angular/core';
 import { DashboardWidget } from "../../widgets/dashboard-widget/dashboard-widget";
 import { DashboardService } from './services/dashboard-service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { wrapGrid } from 'animate-css-grid'
 
 
 @Component({
@@ -13,7 +14,11 @@ import { MatMenuModule } from '@angular/material/menu';
   styleUrl: './dashboard.scss',
   providers: [DashboardService]
 })
-export default class Dashboard {
+export default class Dashboard implements OnInit {
   dashboardService = inject(DashboardService);
-  
+  dashboard = viewChild.required<ElementRef>('dashboard');
+
+  ngOnInit(): void {
+    wrapGrid(this.dashboard().nativeElement, {duration: 300})
+  }
 }
